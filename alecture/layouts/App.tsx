f71ba60e5cda62bd74cbf7c062a1cce394887d38
umcondo <superpowerkdg@gmail.com>
2022-06-23 14:49:38 +0900

    Add code spliting, emotion

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-// components
-import Login from '@pages/Login';
-import SignUp from '@pages/SignUp';
+// components, code spliting - 페이지단위로
+const LogIn = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="*" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/LogIn" replace />} />
+          <Route path="/LogIn" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
--- alecture/webpack.config.ts
+++ alecture/webpack.config.ts
@@ -43,7 +43,10 @@ const config: webpack.Configuration = {
           ],
           env: {
             development: {
-              plugins: [require.resolve('react-refresh/babel')],
+              plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
+            },
+            production: {
+              plugins: ['@emotion'],
             },
           },
         },
