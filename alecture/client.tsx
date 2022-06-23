6c256439714c1c78f4f82d810019aa007ab2a711
umcondo <superpowerkdg@gmail.com>
2022-06-23 14:23:54 +0900

    Add 폴더구조 및 라우터설정

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './layouts/App';
+import App from '@layouts/App';
 
 render(<App />, document.querySelector('#App'));
+
+/* page 구조
+    pages - 서비스 페이지
+    components - 짜잘 컴포넌트
+    layouts - 공통 레이아웃
+*/
--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// components
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>좀만 참자 ㅋㅋ 힘들당</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
